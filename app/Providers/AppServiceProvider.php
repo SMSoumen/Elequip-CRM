fcd1090262cc1baeb802790b30ebee52f2b70070
SMSoumen <soumen@smsolutions.in>
2025-01-06 19:59:38 +0530

    auto google drive backup implemented

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,13 +2,16 @@
 
 namespace App\Providers;
 
-use App\Models\Category;
-use App\Models\Contact;
-use Illuminate\Support\Facades\Blade;
-use Illuminate\Support\Facades\Cache;
+// use App\Models\Category;
+// use App\Models\Contact;
+// use Illuminate\Support\Facades\Blade;
+// use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Gate;
-use Illuminate\Support\Facades\View;
+// use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
+
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -29,6 +32,38 @@ class AppServiceProvider extends ServiceProvider
         Gate::before(function ($user, $ability) {
             return $user->hasRole('Super-Admin') ? true : null;
         });
-        
+
+        $this->loadGoogleDriveAdapter(config('filesystems.disks.google'));
+    }
+
+    private function loadGoogleDriveAdapter($config)
+    {
+        try {
+            Storage::extend('google', function ($app, $config) {
+                $options = [];
+
+                if (!empty($config['teamDriveId'] ?? null)) {
+                    $options['teamDriveId'] = $config['teamDriveId'];
+                }
+
+                // if (!empty($config['sharedFolderId'] ?? null)) {
+                //     $options['sharedFolderId'] = $config['sharedFolderId'];
+                // }
+
+                $client = new \Google\Client();
+                $client->setClientId($config['clientId']);
+                $client->setClientSecret($config['clientSecret']);
+                $client->refreshToken($config['refreshToken']);
+
+                $service = new \Google\Service\Drive($client);
+                $adapter = new \Masbug\Flysystem\GoogleDriveAdapter($service, $config['folder'] ?? '/', $options);
+                $driver = new \League\Flysystem\Filesystem($adapter);
+
+                return new \Illuminate\Filesystem\FilesystemAdapter($driver, $adapter);
+            });
+        } catch (\Exception $e) {
+            Log::error($e->getMessage());
+            // your exception handling logic
+        }
     }
 }
--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,16 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
+// use Illuminate\Foundation\Inspiring;
+// use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+// Artisan::command('inspire', function () {
+//     $this->comment(Inspiring::quote());
+// })->purpose('Display an inspiring quote')->hourly();
+
+
+// Schedule::command('backup:run')->dailyAt('22:00')->timezone('Asia/Kolkata')->withoutOverlapping(10);
+// Schedule::command('backup:clean')->weeklyOn(1, '6:00')->timezone('Asia/Kolkata')->withoutOverlapping(10);
+
+Schedule::command('backup:run')->everyMinute()->withoutOverlapping(10);
+Schedule::command('backup:clean')->everyTwoMinutes()->withoutOverlapping(10);
